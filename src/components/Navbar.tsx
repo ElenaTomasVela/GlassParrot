1285fa0f94204440b7a98f5803aec118b39982de
Elena Tomás Vela <etomasvela@gmail.com>
2026-05-12 23:59:14 +0200

    feat: add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import { Separator } from "./Separator";
 import type { ReactNode } from "react";
+import LogoSvg from "/public/GlassParrot.svg";
 
 const NavLink = ({ to, children }: { to: string; children: ReactNode }) => {
   return (
@@ -18,7 +19,12 @@ const NavLink = ({ to, children }: { to: string; children: ReactNode }) => {
 export const Navbar = () => {
   return (
     <>
-      <div className="flex gap-2 px-8 py-2">
+      <div className="flex gap-2 px-8 py-2 items-center">
+        <Link to="/" className="flex items-center text-sm font-black gap-2">
+          <img src={LogoSvg} className="size-10 my-1" />
+          <span>GlassParrot</span>
+        </Link>
+
         <NavLink to="/">Inicio</NavLink>
         <NavLink to="/tutorial">Tutorial</NavLink>
         <NavLink to="/evaluation">Ponte a prueba</NavLink>
